f0867e75eed3c6c7ad3bc7dd2738ae1713a2404a
huytd <huy.td173171@sis.hust.edu.vn>
2023-04-17 22:36:08 +0700

    init project

---
+++ src/main/java/com/project/shopmobile/ShopMobileApplication.java
@@ -0,0 +1,13 @@
+package com.project.shopmobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopMobileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopMobileApplication.class, args);
+	}
+
+}
