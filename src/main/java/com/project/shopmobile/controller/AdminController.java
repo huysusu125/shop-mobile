eee36b33be91f569e5eaf1aefc564c36254afc88
huytd <huy.td173171@sis.hust.edu.vn>
2023-05-09 00:15:32 +0700

    cors

--- src/main/java/com/project/shopmobile/controller/AdminController.java
+++ src/main/java/com/project/shopmobile/controller/AdminController.java
@@ -1,30 +1,30 @@
-package com.project.shopmobile.controller;
-
-import com.project.shopmobile.service.ImageService;
-import com.project.shopmobile.service.ItemsService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-
-@RequestMapping("/admin")
-@RestController
-public class AdminController {
-
-    private final ItemsService itemsService;
-
-    private final ImageService imagesService;
-
-    public AdminController(ItemsService itemsService, ImageService imagesService) {
-        this.itemsService = itemsService;
-        this.imagesService = imagesService;
-    }
-
-    @PostMapping()
-    public ResponseEntity<?> uploadImages(@RequestParam("file") MultipartFile file) {
-        return ResponseEntity.ok(imagesService.uploadImageToFlickr(file));
-    }
-
-}
+//package com.project.shopmobile.controller;
+//
+//import com.project.shopmobile.service.ImageService;
+//import com.project.shopmobile.service.ItemsService;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.multipart.MultipartFile;
+//
+//@RequestMapping("/admin")
+//@RestController
+//public class AdminController {
+//
+//    private final ItemsService itemsService;
+//
+//    private final ImageService imagesService;
+//
+//    public AdminController(ItemsService itemsService, ImageService imagesService) {
+//        this.itemsService = itemsService;
+//        this.imagesService = imagesService;
+//    }
+//
+//    @PostMapping()
+//    public ResponseEntity<?> uploadImages(@RequestParam("file") MultipartFile file) {
+//        return ResponseEntity.ok(imagesService.uploadImageToFlickr(file));
+//    }
+//
+//}
--- src/main/java/com/project/shopmobile/service/ImageService.java
+++ src/main/java/com/project/shopmobile/service/ImageService.java
@@ -1,21 +1,23 @@
-package com.project.shopmobile.service;
-
-import com.flickr4java.flickr.Flickr;
-import com.flickr4java.flickr.REST;
-import com.flickr4java.flickr.auth.Auth;
-import com.flickr4java.flickr.auth.Permission;
-import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
-
-@Service
-public class ImageService {
-    private final Flickr flickr = new Flickr("a6f556c3484074beb37110f219675986", "229ae65c5e9d2c86", new REST());
-    public Object uploadImageToFlickr(MultipartFile file) {
-//        Auth auth = new Auth();
-//        auth.setPermission(Permission.DELETE);
-//        auth.setToken(token);
-//        auth.setTokenSecret(tokenSecret);
-//        flickr.setAuth(auth);
-        return null;
-    }
-}
+//package com.project.shopmobile.service;
+//
+//import com.flickr4java.flickr.Flickr;
+//import com.flickr4java.flickr.REST;
+//import com.flickr4java.flickr.auth.Auth;
+//import com.flickr4java.flickr.auth.Permission;
+//import org.springframework.stereotype.Service;
+//import org.springframework.web.multipart.MultipartFile;
+//
+//@Service
+//public class ImageService {
+//    private final String apiKey = "a6f556c3484074beb37110f219675986";
+//    private final
+//    private final Flickr flickr = new Flickr("a6f556c3484074beb37110f219675986", "229ae65c5e9d2c86", new REST());
+//    public Object uploadImageToFlickr(MultipartFile file) {
+////        Auth auth = new Auth();
+////        auth.setPermission(Permission.DELETE);
+////        auth.setToken(token);
+////        auth.setTokenSecret(tokenSecret);
+////        flickr.setAuth(auth);
+//        return null;
+//    }
+//}
